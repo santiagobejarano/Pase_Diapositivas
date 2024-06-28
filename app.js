ead78583e3cab8d9b90d31798651df62b8f3bb04
Santiago Bejarano <santiago.bejarano@epn.edu.ec>
2024-06-27 19:23:27 -0500

    feat: Mejora de código JS
    Pequeño cambio en styless.css para

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
 var direccionImagenes = "diapositivas";
 var numeroSlides = document.querySelectorAll("#lista ul li").length;
 var nombreImagen = "Diapositiva";
-var extensionImgagen = ".jpeg";
+var extensionImagen = ".jpeg";
 var cuadroImagen = document.querySelector("#container img");
 var btnAvanzar = document.querySelector("#btnAvanzar");
 var btnRetroceder = document.querySelector("#btnRetroceder");
 var listaSlidesSelectorArray = document.querySelectorAll("#lista ul li");
 var contador = 1;
 
+cuadroImagen.setAttribute("src", `${direccionImagenes}/${nombreImagen}${contador}${extensionImagen}`);
+
+document.getElementById("totalSlides").textContent = numeroSlides;
+actualizarEstadoInfo();
+
+btnAvanzar.addEventListener("click", function() {
+    if (contador < numeroSlides) {
+        contador++;
+        cuadroImagen.setAttribute("src", `${direccionImagenes}/${nombreImagen}${contador}${extensionImagen}`);
+        actualizarEstadoInfo();
+    }
+});
+
+btnRetroceder.addEventListener("click", function() {
+    if (contador > 1) {
+        contador--;
+        cuadroImagen.setAttribute("src", `${direccionImagenes}/${nombreImagen}${contador}${extensionImagen}`);
+        actualizarEstadoInfo();
+    }
+});
+
+listaSlidesSelectorArray.forEach((element, indice) => {
+    element.addEventListener("click", function() {
+        contador = indice + 1;
+        cuadroImagen.setAttribute("src", `${direccionImagenes}/${nombreImagen}${contador}${extensionImagen}`);
+        actualizarEstadoInfo();
+    });
+});
+
+function actualizarEstadoInfo() {
+    document.getElementById("slideActual").textContent = contador;
+
+    btnAvanzar.disabled = contador === numeroSlides;
+    btnRetroceder.disabled = contador === 1;
+
+    if (contador > 1 && contador < numeroSlides) {
+        btnAvanzar.disabled = false;
+        btnRetroceder.disabled = false;
+    }
+}
+
+/* Movimiento barra lateral */
 const botonBarraLateral = document.querySelector(".toggle-btn");
 const lista = document.querySelector("#lista");
 const contenedorImagen = document.querySelector("#container");
@@ -18,53 +60,3 @@ botonBarraLateral.addEventListener("click", function() {
     contenedorImagen.classList.toggle("moved");
     controles.classList.toggle("moved");
 });
-
-cuadroImagen.setAttribute("src",direccionImagenes + "/" + nombreImagen + contador + extensionImgagen);
-
-document.getElementById("totalSlides").textContent = numeroSlides;
-actualizarEstadoInfo();
-
-btnAvanzar.addEventListener("click", function(){
-    if(contador < numeroSlides){
-        contador ++;
-        cuadroImagen.setAttribute("src",direccionImagenes + "/" + nombreImagen + contador + extensionImgagen);
-        actualizarEstadoInfo();
-    }
-});
-
-btnRetroceder.addEventListener("click", function(){
-    if(contador > 1){
-        contador --;
-        cuadroImagen.setAttribute("src",direccionImagenes + "/" + nombreImagen + contador + extensionImgagen);
-        actualizarEstadoInfo();
-    }
-});
-
-listaSlidesSelectorArray.forEach((element, indice) => {
-    element.addEventListener("click",function(){
-        contador = indice+1;
-        cuadroImagen.setAttribute("src",direccionImagenes + "/" + nombreImagen + contador + extensionImgagen);
-        actualizarEstadoInfo();
-        console.log(contador);
-    })
-});
-
-function actualizarEstadoInfo(){
-    document.getElementById("slideActual").textContent = contador;
-    if(contador == numeroSlides){
-        btnAvanzar.disabled = true;
-        if(btnRetroceder.hasAttribute("disabled"))
-            btnRetroceder.removeAttribute("disabled");
-    }
-    if(contador ==1){
-        btnRetroceder.disabled = true;
-        if(btnAvanzar.hasAttribute("disabled"))
-            btnAvanzar.removeAttribute("disabled");
-    }
-    if(contador>1 && contador <numeroSlides){
-        if(btnAvanzar.hasAttribute("disabled"))
-            btnAvanzar.removeAttribute("disabled");
-        if(btnRetroceder.hasAttribute("disabled"))
-            btnRetroceder.removeAttribute("disabled");
-    }
-}
